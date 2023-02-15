85596ce7c5abddd5e0a09fff0f5d31f0695d7552
jackzhhuang <jackzhhuang@tencent.com>
2023-02-16 01:37:40 +0800

    finish all

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let items = s.split(",").collect::<Vec<&str>>();
+        if items.len() != 2 {
+            return Person::default();
+        }
+        if items[0].len() == 0 {
+            return Person::default();
+        }
+        let age_result = items[1].parse::<usize>(); 
+        if let Err(_) = age_result {
+            return Person::default();
+        }
+        Person {
+            name: items[0].to_string(),
+            age: age_result.unwrap(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let items = s.split(",").collect::<Vec<&str>>();
+        if items.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if items[0].len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+        let age_result = items[1].parse::<usize>(); 
+        if let Err(e) = age_result {
+            return Err(ParsePersonError::ParseInt(e));
+        }
+        Ok(Person {
+            name: items[0].to_string(),
+            age: age_result.unwrap(),
+        })
     }
 }
 
